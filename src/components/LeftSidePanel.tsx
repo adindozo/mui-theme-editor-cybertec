6cc34b2e25373a172c54384fca54bac1985712b4
adindozo <adin.dozo1@outlook.com>
2025-02-25 21:27:31 +0100

    add layout for side panel and proper desktop view of website in theme editor

--- src/components/LeftSidePanel.tsx
+++ src/components/LeftSidePanel.tsx
@@ -1,5 +1,14 @@
+import { Box } from "@mui/material";
 import React from "react";
 
 export const LeftSidePanel = () => {
-  return <div>LeftSidePanel</div>;
+  return (
+    <Box
+      sx={{
+        width: "20%",
+        height: "calc(100vh-640px)",
+        backgroundColor: "#f0f0f0",
+      }}
+    ></Box>
+  );
 };
--- src/pages/CustomizeThemePage.tsx
+++ src/pages/CustomizeThemePage.tsx
@@ -4,26 +4,60 @@ import HomePage from "./HomePage";
 import { UpperPanel } from "../components/UpperPanel";
 import { LeftSidePanel } from "../components/LeftSidePanel";
 import { ThemeContext } from "../App";
-
+import { DeviceFrameset } from "react-device-frameset";
+import "react-device-frameset/styles/marvel-devices.min.css";
 const CustomizeThemePage = () => {
   const context = useContext(ThemeContext);
   if (!context) return null;
   const { view } = context;
+
+  const deviceMap = {
+    desktop: { device: "MacBook Pro", width: "100%", height: "100%" },
+    mobile: { device: "iPhone 8", width: "430px", height: "932px" },
+  };
+
+  const { device, width, height } = deviceMap[view] || deviceMap.desktop;
+
   return (
     <>
       <UpperPanel />
-      <LeftSidePanel />
-      <Box
-        key={view}
-        sx={{
-          width: view === "desktop" ? "100%" : "430px",
-          height: view === "desktop" ? "100%" : "932px",
-          overflowY: "auto",
-
-          transition: "width 0.3s ease-in-out, margin 0.3s ease-in-out",
-        }}
-      >
-        <HomePage />
+      <Box sx={{ display: "flex", height: "calc(100vh-64px)" }}>
+        <LeftSidePanel />
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "calc(100vh-64px)",
+          }}
+        >
+          {view === "mobile" ? (
+            <DeviceFrameset device={"iPhone 8"}>
+              <Box
+                sx={{
+                  width,
+                  height,
+                  overflowY: "auto",
+                }}
+              >
+                <HomePage />
+              </Box>
+            </DeviceFrameset>
+          ) : (
+            <Box
+              key={view}
+              sx={{
+                width: "100%", // Full screen for desktop view
+                height: "calc(100vh - 64px)", // Subtract height of AppBar
+                overflowY: "auto",
+                transition: "width 0.3s ease-in-out, margin 0.3s ease-in-out",
+              }}
+            >
+              <HomePage />
+            </Box>
+          )}
+        </Box>
       </Box>
     </>
   );
